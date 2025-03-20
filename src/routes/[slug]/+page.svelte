c806b9cf38e7bd850a0510208271ab46b2db0643
yelly <kk3p1q1@gmail.com>
2025-03-20 23:59:49 +0900

    add dynamin routing

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let data;
+	console.log(data);
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article>
+	<div>
+		<h1>{data.meta.title}</h1>
+		<p>Published at {data.meta.date}</p>
+	</div>
+
+	<div>
+		<svelte:component this={data.content} />
+	</div>
+</article>
