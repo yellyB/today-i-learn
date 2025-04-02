c4f8bf015a8baacbd996f13cdf49268d156e435f
yelly <kk3p1q1@gmail.com>
2025-04-02 17:19:11 +0900

    vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,6 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
-const dev = process.argv.includes('dev');
-const base = dev ? '' : '/today-i-learned';
-
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
   extensions: ['.md']
@@ -21,14 +18,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      pages: 'build',  // ✅ 빌드 폴더를 'build'로 설정
-      assets: 'build',
-      fallback: 'index.html',
-    }),
-    paths: {
-      base
-    },
+    adapter: adapter()
   }
 };
 
